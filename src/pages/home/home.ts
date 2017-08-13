aaf52b0780e6294e2f2eae5403d6d057abc7a1bf
Rupesh Tiwari <roopkt@gmail.com>
2017-08-13 09:19:00 -0400

    displaying 3 people

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,43 +6,121 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  person = {
-    gender: "female",
-    name: {
-      title: "ms",
-      first: "elif",
-      last: "tazegül"
+
+  people = [
+    {
+      gender: "female",
+      name: {
+        title: "ms",
+        first: "micaela",
+        last: "freitas"
+      },
+      location: {
+        street: "3337 rua são josé ",
+        city: "sertãozinho",
+        state: "mato grosso",
+        postcode: 29533
+      },
+      email: "micaela.freitas@example.com",
+      login: {
+        username: "bluegoose608",
+        password: "highbury",
+        salt: "61dpXvrC",
+        md5: "eb597e3a5f62086d513516cf857d8032",
+        sha1: "5aee5fa7a84c467500f496096a3b96aa9e719904",
+        sha256: "c4a9f3494c63a29ab58fe3dc08d737f0c59ff7ab68584a2e8df8f1b69937a4ea"
+      },
+      dob: "1973-10-17 00:49:57",
+      registered: "2008-03-01 15:27:52",
+      phone: "(69) 0619-0626",
+      cell: "(56) 0359-2302",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/women/68.jpg",
+        medium: "https://randomuser.me/api/portraits/med/women/68.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/women/68.jpg"
+      },
+      nat: "BR"
     },
-    location: {
-      street: "9513 filistin cd",
-      city: "bolu",
-      state: "ardahan",
-      postcode: 72479
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "manuel",
+        last: "garrido"
+      },
+      location: {
+        street: "8336 avenida de castilla",
+        city: "elche",
+        state: "comunidad valenciana",
+        postcode: 80757
+      },
+      email: "manuel.garrido@example.com",
+      login: {
+        username: "beautifuldog350",
+        password: "corwin",
+        salt: "z8v2xR9D",
+        md5: "5f2c0e9499189efcb4172ac7f79ebf1c",
+        sha1: "bf4cc363ee735c3f649e3706e5731bf2df42df4d",
+        sha256: "e6dbe5dbe560354636a80802ecd08443bb7b3e91083a0a6e207b6ee8c80be580"
+      },
+      dob: "1975-10-28 05:30:04",
+      registered: "2005-05-03 02:14:27",
+      phone: "915-582-079",
+      cell: "629-049-727",
+      id: {
+        name: "DNI",
+        value: "01893843-M"
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/9.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/9.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/9.jpg"
+      },
+      nat: "ES"
     },
-    email: "elif.tazegül@example.com",
-    login: {
-      username: "beautifulbear727",
-      password: "steven",
-      salt: "aqaLlEZD",
-      md5: "b311c8f528e06852ab95c0e62b3da648",
-      sha1: "a4d021a1809c1840694bb0407d9f4f71c73417c9",
-      sha256: "45dde9ec5e73b7ea8f6cbd1ce955784e52e68b07dbf2ae0721deb91c46f96b1f"
-    },
-    dob: "1964-03-15 19:51:39",
-    registered: "2004-12-22 00:29:23",
-    phone: "(720)-302-8977",
-    cell: "(663)-069-9393",
-    id: {
-      name: "",
-      value: null
-    },
-    picture: {
-      large: "https://randomuser.me/api/portraits/women/50.jpg",
-      medium: "https://randomuser.me/api/portraits/med/women/50.jpg",
-      thumbnail: "https://randomuser.me/api/portraits/thumb/women/50.jpg"
-    },
-    nat: "TR"
-  }
+    {
+      gender: "male",
+      name: {
+        title: "mr",
+        first: "theo",
+        last: "mackay"
+      },
+      location: {
+        street: "6947 stanley way",
+        city: "sidney",
+        state: "alberta",
+        postcode: 42573
+      },
+      email: "theo.mackay@example.com",
+      login: {
+        username: "purpleleopard946",
+        password: "matty",
+        salt: "eQCQwJyd",
+        md5: "a96b2c8f3d7685122faf84560e80ff92",
+        sha1: "e5376a657797fb015da0525829a4effd52f06e9e",
+        sha256: "c6d3e62545b8295ad25fe49c56406b91eb6522bb86f5a8f8827cc8ee3919627e"
+      },
+      dob: "1961-05-21 06:37:55",
+      registered: "2008-08-04 13:40:14",
+      phone: "857-100-2413",
+      cell: "929-992-9865",
+      id: {
+        name: "",
+        value: null
+      },
+      picture: {
+        large: "https://randomuser.me/api/portraits/men/85.jpg",
+        medium: "https://randomuser.me/api/portraits/med/men/85.jpg",
+        thumbnail: "https://randomuser.me/api/portraits/thumb/men/85.jpg"
+      },
+      nat: "CA"
+    }
+  ];
+
   constructor(public navCtrl: NavController) {
 
   }
